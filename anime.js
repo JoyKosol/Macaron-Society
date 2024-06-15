040cd495b53c242521c8ea4614648724b68d41fb
JoyKosol <169111886+JoyKosol@users.noreply.github.com>
2024-06-15 16:06:27 -0700

    anime.js

--- anime.js
+++ anime.js
@@ -1,1 +1,1 @@
-alert("Hi there!! 🤗 This an individual project about creating wesite by using HTML and CSS.👩🏻‍💻 The content is about macarons, my favourite dessert!😍 Feel free to browse it!😘");
+alert(Hi there! 🤗 This is an individual project about creating a website using HTML and CSS. 👩🏻‍💻 The content is about macarons, my favorite dessert! 😍 Feel free to browse it!😘");
